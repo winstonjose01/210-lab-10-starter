603937259be0643abe9d6fc44fa6dbf6f23f0c52
winstonjose01 <winston.jose@gmail.com>
2024-10-02 09:49:29 -0700

    Initial

--- main.cpp
+++ main.cpp
@@ -4,6 +4,17 @@ using namespace std;
 int main() {
 
     // add any code here, sort of a 'hello world plus' code block
+    time_t now = time(0);  // Get the current time
+    tm* localtm = localtime (&now); // Get the local time
+    
+    cout << "\nHello World!";
+    cout << "\nThis is a sample program for Lab 10 - Fork This Repo";
+    int year = localtm -> tm_year + 1900; 
+    int month = localtm -> tm_mon + 1;
+    int day = localtm -> tm_mday;
+
+    cout << "\nDate: " << day << "-" << month << "-" << year << endl;
+
 
     return 0;
 }
